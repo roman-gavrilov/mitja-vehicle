dda5a4d67d29e557d533f6c02c8972b71daf32e8
Roman <roman.gavrilov.0309@gmail.com>
2024-10-02 15:12:07 -0400

    dep2

--- src/app/components/dashboard/overview/dashboardSections.js
+++ src/app/components/dashboard/overview/dashboardSections.js
@@ -4,7 +4,7 @@ import { HomeIcon } from "@heroicons/react/24/outline";
 const DashboardCard = ({ title, description, icon }) => {
   return (
     <div className="bg-white p-6 rounded-lg shadow-md flex flex-col items-center text-center">
-      <div className="text-5xl mb-4"><img src={icon} alt={title} /></div>
+      <div className="text-5xl mb-4"><img width={40} height={40} src={icon} alt={title} /></div>
       <h2 className="text-xl font-bold">{title}</h2>
       <p className="text-gray-500">{description}</p>
     </div>
@@ -12,18 +12,39 @@ const DashboardCard = ({ title, description, icon }) => {
 };
 
 const DashboardSections = async () => {
-  const client = createClient();
-  const { data } = await client.getSingle("admin_bashboard");
+  const data = [
+    {
+      box_title: "Start a new search",
+      box_content: "0 vehicles",
+      icon: {
+        url: "/images/svgviewer-png-output (2).png"
+      }
+    },
+    {
+      box_title: "View my searches",
+      box_content: "No saved searches",
+      icon: {
+        url: "/images/svgviewer-png-output (1).png"
+      }
+    },
+    {
+      box_title: "Go to my listings",
+      box_content: "0 vehicles",
+      icon: {
+        url: "/images/svgviewer-png-output (3).png"
+      }
+    }
+  ]
 
   return (
     <div className="bg-gray-100 flex flex-col items-center mb-[20px] pb-[20px] border pt-[20px]">
       <h1 className="text-3xl font-bold mb-6">
-        {data?.dashboard_title}
+        Hello, what do you want to do today?
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {
-          data.overview_boxes.map((item, index) => (
+          data.map((item, index) => (
             <DashboardCard
               key={index}
               title={item.box_title}
