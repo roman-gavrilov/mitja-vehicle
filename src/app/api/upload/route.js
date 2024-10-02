2ba3b287803b61b07ac6696e527e70fdcc61a3a4
Roman <roman.gavrilov.0309@gmail.com>
2024-10-02 19:49:21 -0400

    reduced more quality

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -12,9 +12,10 @@ export async function POST(req) {
     try {
       const resizedImageBuffer = await sharp(buffer)
         .resize(800) // Resize to 800px width, maintaining aspect ratio
+        .jpeg({ quality: 80 })
         .toBuffer();
 
-      const base64Image = `data:image/${file.type};base64,${resizedImageBuffer.toString('base64')}`;
+      const base64Image = `data:image/jpeg;base64,${resizedImageBuffer.toString('base64')}`;
       resizedImages.push(base64Image);
     } catch (error) {
       console.error('Error processing image:', error);
