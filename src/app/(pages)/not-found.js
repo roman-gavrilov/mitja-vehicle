f092108b4f04fec1077f75dccab1f2ce45a8ac11
Roman <roman.gavrilov.0309@gmail.com>
2024-10-24 18:39:54 -0400

    removed windows object

--- src/app/(pages)/not-found.js
+++ src/app/(pages)/not-found.js
@@ -1,8 +1,6 @@
 import { redirect } from 'next/navigation'
 
-export default function NotFound() {
-  const path = window?.location?.pathname || '';
-  
+export default function NotFound() {  
   // Redirect to dashboard 404 to maintain dashboard layout
-  redirect(`/dashboard${path}`);
+  redirect(`/dashboard/`);
 }
